945187985f97a93db16cbca7cb7bf8e4d1c5157c
Justin Fry <mrjustinfry@gmail.com>
2019-04-15 14:21:30 -0400

    shop now button

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
         return (
             <div className="introBox">
                 <h2 className="introHead">Matt's Coaching</h2>
-                <p className="introInfo">Beginner Plans starting at only <span className="introPrice">$39<span className="cents">.00</span></span>!</p>
+                <p className="introInfo">Beginner Plans starting at only <span className="introPrice">$39<span className="cents">.00</span></span></p>
                 <div className="linkBox"><Link to="/plan" className="shopPlansBtn">Shop Plans</Link></div>
             </div>
         )
